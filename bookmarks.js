a24ddc1905ad9a7872e8be498752e1c42f8014f8
Natalie Monger <monger@usc.edu>
2017-07-21 23:34:51 -0700

    pulled bookmarks into separate structure

---
+++ bookmarks.js
@@ -0,0 +1,20 @@
+<!-- Bookmarks -->
+var bookmark1 = {
+	title: 'USC',
+	pano:  'CAoSLEFGMVFpcFBuZG9iRTRuNVdvMUo2cEV0bkN2S1poVXlVUmxPakVJeDZsajlw',
+	heading: 0
+}; 
+		
+var bookmark2 = {
+	title: 'Sydney',
+	pano: 'E045SvTR9OoV1Sh14Fpt0A',
+	heading: 0
+};
+		
+var bookmark3 = {
+	title: 'Disneyland',
+	pano: 'CAoSLEFGMVFpcE1zYXo0bjJxVHRPNEpSSUpIaUU5ZzY5aGoxZHZKQjVSQmI0dHNj',
+	heading: 45
+};
+		
+bookmarks = [bookmark1, bookmark2, bookmark3]; 
